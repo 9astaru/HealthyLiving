08cc41d5bc6841a6779102c7b248def4cf203823
Akash S Taru <107749594+9astaru@users.noreply.github.com>
2023-06-13 00:56:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navb from './Components/Navbar';
+import Herose from './Components/Herose';
+
+function App() {
+  return (
+    <div classNameName="App">
+      <Navb />
+      <Herose />
+    </div>
+  );
+}
+
+export default App;
